85dcd7847a6f23e20ca3a40326ee542e73deeb46
Jonathan Häberle <jonathan.haeberle@gmail.com>
2016-02-08 15:42:56 +0100

    initial commit

---
+++ fixtures/all3dp-print-service.d.ts
@@ -0,0 +1,214 @@
+export interface Offer {
+materialConfigurationId : string;
+price : {
+net : number;
+state : string;
+hasPrice : boolean;
+errorMsg : string;
+currency : string;
+shipping : number;
+total : number;
+tax : number;
+}
+}
+export interface MaterialConfiguration extends Array<{
+printingService : string;
+color : string;
+material : {
+name : string;
+description : string;
+}
+finish : {
+name : string;
+description : string;
+}
+materialConfigurationId : string;
+finishGroup : {
+name : string;
+description : string;
+}
+materialGroup : {
+name : string;
+description : string;
+}
+}> {}
+export interface Address {
+lastName : string;
+firstName : string;
+countryCode : string;
+}
+export interface Model {
+modelId : string;
+designerCommissonCurrency : string;
+designerCommisson : number;
+downloadUrl : string;
+mesh : {
+area : number;
+boundingBox : {
+z : number;
+x : number;
+y : number;
+}
+volume : number;
+}
+unit : string;
+filename : string;
+}
+export interface Price {
+net : number;
+state : string;
+hasPrice : boolean;
+errorMsg : string;
+currency : string;
+shipping : number;
+total : number;
+tax : number;
+}
+export interface CartItem {
+amount : number;
+modelId : string;
+scale : number;
+materialConfigurationId : string;
+}
+export interface FetchModelParameters {
+modelLocation : string;
+unit : string;
+sessionToken : string;}
+export interface GetOffersParameters {
+modelId : string;
+queryMode : string;
+scale : number;
+currency : string;
+countryCode : string;
+promotionCode : string;
+materialGroupFilter : string;
+materialFilter : string;
+finishGroupFilter : string;
+printingServiceFiler : string;
+colorFilter : string;
+sessionToken : string;}
+export interface CreateOrderWithStripeParameters {
+configurations : {
+totalPrice : number;
+address : {
+lastName : string;
+firstName : string;
+countryCode : string;
+}
+currency : string;
+cartItems : {
+amount : number;
+modelId : string;
+scale : number;
+materialConfigurationId : string;
+}[];
+stripeToken : string;
+promotionCode : string;
+}}
+export interface EstimateOffersParameters {
+volume : number;
+surface : number;
+bboxX : number;
+bboxY : number;
+bboxZ : number;
+currency : string;
+materialGroupFilter : string;
+materialFilter : string;
+finishGroupFilter : string;
+printingServiceFiler : string;
+colorFilter : string;}
+export interface GetOrderStatusParameters {
+orderId : string;}
+export interface GetAllModelsParameters {
+sessionToken : string;}
+export interface GetOfferParameters {
+modelId : string;
+materialConfigurationId : string;
+queryMode : string;
+scale : number;
+countryCode : string;
+currency : string;
+promotionCode : string;
+sessionToken : string;}
+export interface SubscribeToNewOffersParameters {
+modelId : string;
+scale : number;
+currency : string;
+countryCode : string;
+promotionCode : string;}
+export interface CreateOrderWithPaypalParameters {
+configurations : {
+totalPrice : number;
+address : {
+lastName : string;
+firstName : string;
+countryCode : string;
+}
+currency : string;
+cartItems : {
+amount : number;
+modelId : string;
+scale : number;
+materialConfigurationId : string;
+}[];
+promotionCode : string;
+}}
+export interface OrderParameters {
+configurations : {
+apiKey : string;
+totalPrice : number;
+address : {
+lastName : string;
+firstName : string;
+countryCode : string;
+}
+secretKey : string;
+currency : string;
+cartItems : {
+amount : number;
+modelId : string;
+scale : number;
+materialConfigurationId : string;
+}[];
+promotionCode : string;
+}}
+export interface GetMaterialConfigurationsParameters {
+materialGroupFilter : string;
+materialFilter : string;
+finishGroupFilter : string;
+printingServiceFiler : string;
+colorFilter : string;}
+export interface UploadModelParameters {
+file : File;
+unit : string;
+sessionToken : string;}
+export interface GetCartParameters {
+configurations : {
+address : {
+lastName : string;
+firstName : string;
+countryCode : string;
+}
+currency : string;
+countryCode : string;
+configuration : {
+amount : number;
+modelId : string;
+scale : number;
+materialConfigurationId : string;
+}[];
+promotionCode : string;
+}}
+export interface GetModelParameters {
+modelId : string;
+sessionToken : string;}
+export interface DeleteModelParameters {
+sessionToken : string;
+modelId : string;}
+export interface ChangeModelParameters {
+modelId : string;
+model : {
+sessionToken : string;
+commissonCurrency : string;
+commisson : number;
+}}
---
+++ index.js
@@ -0,0 +1,82 @@
+
+
+function createTypescriptDefinitions(root) {
+
+  const deref = obj => {
+    let selectors = obj['$ref'].split('/');
+    selectors.shift();
+
+    return selectors.reduce((last, cur) => last[cur], root);
+  };
+
+  const objectDef = object => {
+    return '{\n' + Object.keys(object.properties).map(propertyName => {
+        const property = object.properties[propertyName];
+        return `${propertyName} : ${typeDefiniton(property)}`;
+      }).join('\n') + '\n}';
+  };
+
+  const arrayDef = object => {
+    return typeDefiniton(object.items);
+  };
+
+  const typeDefiniton = obj => {
+    if (obj['$ref']) return typeDefiniton(deref(obj));
+    if (obj.schema) return typeDefiniton(obj.schema);
+
+    switch (obj.type) {
+      case 'object':
+        return objectDef(obj);
+      case 'string':
+        return `string;`;
+      case 'number':
+        return `number;`;
+      case 'boolean':
+        return `boolean;`;
+      case 'file':
+        return `File;`;
+      case 'array':
+        return arrayDef(obj) + '[];';
+      default:
+        throw new Error(`type ${obj.type} not expected, ${JSON.stringify(obj, null, ' ')}`)
+    }
+  };
+
+  const modelDefinitions =
+  Object.keys(root.definitions).map(definitionName => {
+    const definition = root.definitions[definitionName];
+
+    switch (definition.type) {
+      case 'object': return `export interface ${definitionName} ${typeDefiniton(definition)}`;
+      case 'array': return `export interface ${definitionName} extends Array<${arrayDef(definition)}> {}`;
+      default: throw new Error(`type ${obj.type} not supported in interface`);
+    }
+
+  }).join('\n');
+
+  const parameterDefinition = obj => {
+    return obj.name + ' : ' + typeDefiniton(obj)
+  };
+
+  const pathDefinitons =
+    Object.keys(root.paths).map(pathName => {
+      const path = root.paths[pathName];
+
+      return Object.keys(path).map(methodName => {
+        const method = path[methodName];
+        let operationId = method.operationId;
+        operationId = operationId.charAt(0).toUpperCase() + operationId.slice(1);
+
+        const definition = method.parameters.map(param => parameterDefinition(param)).join('\n');
+
+        return `export interface ${operationId}Parameters {\n${definition}}`;
+      }).join('\n');
+    }).join('\n');
+
+
+  return [modelDefinitions, pathDefinitons].join('\n');
+}
+
+
+let swagger = require('./fixtures/all3dp-print-service.json');
+console.log(createTypescriptDefinitions(swagger));
